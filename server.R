4095627569570e50045a6088909a478c5d7dc949
nitya-l <nityalagadapati@gmail.com>
2023-03-08 03:34:23 -0800

    Merged Version With Initial Edits

--- server.R
+++ server.R
@@ -1,13 +1,21 @@
 
-pathogen_data <- read_delim("IHME_Data.csv")
+library(dplyr)
+data_i <- read_delim("IHME_Data.csv")
+pathogen_data <- data_i %>% select(-sex_id, -sex_name, -metric_id, -upper, -lower, -measure_id, -cause_id, cause_name) %>% filter(!(location_name %in% c(
+                                                                                                                                                                          "Southeast Asia, East Asia, and Oceania", 
+                                                                                                                                                                          "East Asia", "Democratic People's Republic of Korea",
+                                                                                                                                                                          "Southeast Asia", "Lao People's Democratic Republic", "Marshall Islands",
+                                                                                                                                                                          "Central Europe, Eastern Europe, and Central Asia", 
+                                                                                                                                                                          "Central Asia", "Central Europe", "Eastern Europe", "Republic of Moldova", 
+                                                                                                                                                                          "High-income", "High-income Asia Pacific", "Western Europe",
+                                                                                                                                                                          "Southern Sub-Saharan Africa", "Southern Latin America", 
+                                                                                                                                                                          "High-income North America", "Latin America and Caribbean")), metric_name == "Number")
 
-
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
   #sample output
   output$sample <- renderTable({
     sample <- sample_n(pathogen_data,5)
-    #setnames(sample,old = c("location_name","age_group_name","infectious_syndrome","pathogen","val"), new = c("Location","Age Group","Symptoms","Pathogen","# Estimated Deaths"))
     sample
   })
   
@@ -15,6 +23,8 @@ server <- function(input, output) {
   options_age <- as.list(unique(pathogen_data$age_group_name))
   options_symptoms <- as.list(unique(pathogen_data$infectious_syndrome))
   options_locations <- as.list(unique(pathogen_data$location_name)) 
+  
+  
   #Age options
   output$age <- renderUI({
     selectInput("age", label = "Select Age Group", choices = options_age, selected = "All Ages")
@@ -74,7 +84,131 @@ server <- function(input, output) {
     mean_d <- mean(table_d$val)
     paste("The average number of deaths across all infections and age groups is ",toString(round(mean_d,3)),"in", input$locations)
   })
+  
+  #Pathogen vs Symptoms Bar graph
+  output$bars <- renderPlot ({ 
+    
+    pathogen_data %>%  
+      select(pathogen, infectious_syndrome) %>% 
+      filter(pathogen %in% input$pathogenss) %>% 
+      group_by(pathogen, infectious_syndrome) %>% 
+      summarize(num = n()) %>% 
+      ggplot(aes(num, color = pathogen)) + 
+      geom_bar(position = "dodge", fill = "white")
+    
+  })
+  #Reactive text for pathogen vs symptoms  
+  output$react_pathogen <- renderText({
+    path <- pathogen_data %>%  
+      select(pathogen, infectious_syndrome) %>% 
+      filter(pathogen %in% input$pathogenss)
+    unique_symp <- path %>% select(infectious_syndrome) %>% unique()
+    result <- paste(unique_symp, sep = ",")
+    paste("The symptoms associated with the pathogens selected are:", result)
+  })
+
+  
+  # Filter the data based on user inputs
+  filtered_data <- reactive({
+    pathogen_data %>%
+      filter(location_name == input$location,
+             pathogen %in% input$pathogen_checkbox,
+             age_group_name == input$age_group)
+  })
+  
+  # Render the bargraph and scatterplot
+  output$pathogen_plot <- renderPlot({
+    if(input$location == "Global"){
+    data <- pathogen_data %>%
+      group_by(pathogen) %>%
+      summarize(deaths = sum(val)) %>%
+      arrange(desc(deaths))
+    }
+    else{
+      data <- pathogen_data %>%
+        filter(pathogen %in% input$pathogen_checkbox) %>%
+        group_by(pathogen) %>%
+        summarize(deaths = sum(val)) %>%
+        arrange(desc(deaths))
+    }
+    
+    if (input$plot_type == "Bar Graph") {
+      ggplot(data, aes(x = pathogen, y = deaths, fill = pathogen)) +
+        geom_col() +
+        ggtitle(paste("Pathogens Responsible for Deaths in", input$location)) +
+        xlab("Pathogen") +
+        ylab("Number of Deaths") +
+        theme(plot.title = element_text(hjust = 0.5),
+              axis.text.x = element_text(angle = 45, hjust = 1))
+    } else {
+      ggplot(data, aes(x = pathogen, y = deaths, color = pathogen)) +
+        geom_point() +
+        ggtitle(paste("Pathogens Responsible for Deaths in", input$location)) +
+        xlab("Pathogen") +
+        ylab("Number of Deaths") +
+        theme(plot.title = element_text(hjust = 0.5),
+              axis.text.x = element_text(angle = 45, hjust = 1))
+    }
+  })
+  
+  # Render the max deaths text
+  output$pathogen_max_deaths <- renderText({
+    data <- pathogen_data %>%
+      filter(location_name == input$location,
+             pathogen %in% input$pathogen_checkbox) %>%
+      group_by(pathogen) %>%
+      summarize(deaths = sum(val)) %>%
+      arrange(desc(deaths))
+    
+    max_deaths <- max(data$deaths)
+    max_pathogen <- data %>%
+      filter(deaths == max_deaths) %>%
+      pull(pathogen)
+    
+    paste("The pathogen that causes the most deaths is", max_pathogen, "with", 
+          round(max_deaths, 2), "deaths.")
+  })
+  
+  # Render the table on table page
+  output$pathogen_table <- renderTable({
+    data <- pathogen_data %>%
+      filter(age_group_name == input$age_group) %>%
+      group_by(pathogen) %>%
+      summarize(deaths = sum(val)) %>%
+      arrange(desc(deaths))
+    
+  })
+  
+  # Render the death range text
+  output$death_range <- renderText({
+    data <- pathogen_data %>%
+      group_by(pathogen) %>%
+      summarize(deaths = sum(val)) %>%
+      arrange(desc(deaths))
+    
+    min_deaths <- round(min(data$deaths), 2)
+    max_deaths <- round(max(data$deaths), 2)
+    
+    if (input$age_group == "All Ages") {
+      paste("The minimum deaths across all age groups for the selected location and pathogens is", 
+            round(min_deaths, 2), "and the maximum is", round(max_deaths, 2), ".")
+    } else {
+      age_data <- filtered_data() %>%
+        group_by(pathogen, age_group_name) %>%
+        summarize(deaths = sum(val)) %>%
+        arrange(desc(deaths))
+      
+      min_age_deaths <- round(min(age_data$deaths), 2)
+      max_age_deaths <- round(max(age_data$deaths), 2)
+      
+      paste("The minimum deaths for the selected age group, location, and pathogens is", 
+            round(min_age_deaths, 2), "and the maximum is", round(max_age_deaths, 2),".")
+    }
+  })
 }
+
+
+
 
 
 # Run the application 
--- ui.R
+++ ui.R
@@ -15,7 +15,17 @@ library(tidyverse)
 library(data.table)
 
 #read data 
-pathogen_data <- read_delim("IHME_Data.csv")
+data_i <- read_delim("IHME_Data.csv")
+pathogen_data <- data_i %>% select(-sex_id, -sex_name, -metric_id, -upper, -lower, -measure_id, -cause_id, cause_name) %>% filter(!(location_name %in% c(
+                                                                                                                                                                          "Southeast Asia, East Asia, and Oceania", 
+                                                                                                                                                                          "East Asia", "Democratic People's Republic of Korea",
+                                                                                                                                                                          "Southeast Asia", "Lao People's Democratic Republic", "Marshall Islands",
+                                                                                                                                                                          "Central Europe, Eastern Europe, and Central Asia", 
+                                                                                                                                                                          "Central Asia", "Central Europe", "Eastern Europe", "Republic of Moldova", 
+                                                                                                                                                                          "High-income", "High-income Asia Pacific", "Western Europe",
+                                                                                                                                                                          "Southern Sub-Saharan Africa", "Southern Latin America", 
+                                                                                                                                                                          "High-income North America", "Latin America and Caribbean")),metric_name == "Number")
+options_pathogens <- as.list(unique(pathogen_data$pathogen))
 
 # Define UI for application
 ui <- fluidPage(
@@ -31,8 +41,58 @@ ui <- fluidPage(
              p("\n Below is a small random sample of the pathogen data set where val is equal to the # of estimated deaths."), 
              tableOutput("sample"), 
     ), 
-    #Plot page
-    tabPanel("Plot",
+    #Pathogen and Death Rate
+    tabPanel("Pathogen and Associated Number of Deaths by Age",
+             fluidRow(
+               column(
+                 width = 4,
+                 p("Select a specific age group to display a table that provides 
+                   the type of pathogen and the number of deaths it causes by age."),
+                 selectInput(inputId = "age_group", 
+                             label = "Select age group", 
+                             choices = c("All Ages", unique(pathogen_data$age_group_name)), selected = "All Ages"),
+                 textOutput(outputId = "death_range")
+               ),
+               column(
+                 width = 8,
+                 tableOutput(outputId = "pathogen_table")
+               )
+             ),
+    ), 
+
+    #Pathogen and Death rate based on Location 
+    tabPanel("Pathogen and Associated Death Rate by Location ",
+             fluidRow(
+               column(
+                 width = 4,
+                 p("Select a location, various pathogens, and the type of plot to visually 
+                   see how many deaths each pathogen causes based on location."),
+                 selectInput(inputId = "location", 
+                             label = "Select location", 
+                             choices = unique(pathogen_data$location_name)),
+                 checkboxGroupInput(inputId = "pathogen_checkbox",
+                                    label = "Select pathogens to display:",
+                                    choices = unique(pathogen_data$pathogen),
+                                    selected = unique(pathogen_data$pathogen))
+               ),
+               column(
+                 width = 8,
+                 radioButtons(inputId = "plot_type",
+                              label = "Select plot type:",
+                              choices = c("Bar Graph", "Scatterplot"),
+                              selected = "Bar Graph"),
+                 plotOutput(outputId = "pathogen_plot", width = "800px"),
+                 fluidRow(
+                   column(
+                     width = 12,
+                     textOutput(outputId = "pathogen_max_deaths")
+                   )
+                 )
+               )
+             )
+    ),
+    #Infection and Age group
+    tabPanel("Bacterial Infections and Age Groups",
              sidebarLayout(
                sidebarPanel(
                  p("Use the widgets below to select different subsets of data based on certain age groups, or certain combinations of infections/symptoms"),
@@ -43,18 +103,22 @@ ui <- fluidPage(
                  textOutput("react_plot"),
                  plotOutput("plot")),
              ),  
+    ),
+    #Pathogen and Bacterial Infections 
+    tabPanel("Pathogens and Bacterial Infections", 
+             sidebarLayout( 
+               sidebarPanel( 
+                 checkboxGroupInput("pathogenss", "Select Pathogen", choices = options_pathogens, selected = options_pathogens),
+               ),
+               mainPanel ( 
+                 plotOutput("bars"),
+                 textOutput(("react_pathogen"))
+               )
+             ),
+    ),
+    
     ), 
-    #Table page
-    tabPanel("Table",
-             sidebarLayout(
-               sidebarPanel(
-                 p("Select a country using the radio buttons below to see the number of observations for each age group and symptom type."),
-                 uiOutput("locations")), 
-               mainPanel(
-                 textOutput("react_table"),
-                 dataTableOutput("table"))),
-    ), 
-  ),
-)
+  )
+
 # Run the application 
 #shinyApp(ui = ui, server = server)
